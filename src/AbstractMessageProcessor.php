4057dfa86bb42260c8e6d4ef6dfe417163a62438
era269 <era269@ukr.net>
2021-05-18 10:37:58 +0300

    can-process-message-trait

--- src/AbstractMessageProcessor.php
+++ src/AbstractMessageProcessor.php
@@ -4,9 +4,22 @@ declare(strict_types=1);
 
 namespace Era269\MessageProcessor;
 
-use Era269\MessageProcessor\Traits\CanProcessMessageTrait;
+use Era269\MessageProcessor\Message\NullMessage;
+use Era269\MessageProcessor\Traits\CanGetMethodNameByMessageTrait;
+use RuntimeException;
 
 abstract class AbstractMessageProcessor implements MessageProcessorInterface
 {
-    use CanProcessMessageTrait;
+    use CanGetMethodNameByMessageTrait;
+
+    /**
+     * @throws RuntimeException
+     */
+    final public function process(MessageInterface $message): MessageInterface
+    {
+        $methodName = $this->getMethodName($message);
+
+        return $this->$methodName($message)
+            ?? new NullMessage();
+    }
 }
